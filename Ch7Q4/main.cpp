142268e22a7c3c03924f47f5a0523c3315182afe
Ali Elalaoui <alielalaoui@Alis-MacBook-Pro.local>
2018-09-30 12:43:51 -0400

    Commit1

---
+++ Ch7Q4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ch7Q4
+//
+//  Created by Ali Elalaoui on 9/30/18.
+//  Copyright © 2018 Ali Elalaoui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
